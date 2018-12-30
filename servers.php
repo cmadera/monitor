f0909e96bd50c9e0affc7c83c0d5b495ce1c5a76
Carlos Madera <carlos.madera@gmail.com>
2018-12-30 18:41:14 -0200

    Update servers.php

--- servers.php
+++ servers.php
@@ -2,18 +2,23 @@
 // Aplicação de monitoramento de servidores
 // Criada por Carlos Madera - Michelin (17/06/2016)
 
+/*
 $servers = array();
 
-array_push($servers, array('nome' => "WWW",             'url' => "bigbank.com.br",              'agent' => "/monitor/monitor.php", 'protocol' => "https"));
-array_push($servers, array('nome' => "Big",             'url' => "big.bigbank.com.br",          'agent' => "/monitor.php", 'protocol' => "https"));
-array_push($servers, array('nome' => "Big2",            'url' => "big2.bigbank.com.br",         'agent' => "/monitor.php", 'protocol' => "http"));
-array_push($servers, array('nome' => "Test",            'url' => "test.bigbank.com.br",         'agent' => "/monitor.php", 'protocol' => "https"));
-array_push($servers, array('nome' => "Fast",            'url' => "fast.bigbank.com.br",         'agent' => "/monitor.php", 'protocol' => "http"));
-//array_push($servers, array('nome' => "Node1",           'url' => "node1.bigbank.com.br",        'agent' => "/monitor.php", 'protocol' => "https"));
-//array_push($servers, array('nome' => "Node2",           'url' => "node2.bigbank.com.br",        'agent' => "/monitor.php", 'protocol' => "https"));
-//array_push($servers, array('nome' => "Node3",           'url' => "node3.bigbank.com.br",        'agent' => "/monitor.php", 'protocol' => "https"));
-//array_push($servers, array('nome' => "Windows",         'url' => "windows.bigbank.com.br",      'agent' => "/monitor.php", 'protocol' => "http"));
+array_push($servers, array('nome' => "WWW",             'url' => "www.bigbank.com.br",          'agent' => "/monitor.php", 'protocol' => "http"));
+array_push($servers, array('nome' => "APP",				'url' => "app.bigbank.com.br", 	    	'agent' => "/monitor.txt", 'protocol' => "https"));
+array_push($servers, array('nome' => "MQTT",			'url' => "mqtt.bigbank.com.br", 	    'agent' => "/monitor.php", 'protocol' => "http"));
+array_push($servers, array('nome' => "COS701",			'url' => "cos701.bigbank.com.br",  		'agent' => "/monitor.php", 'protocol' => "http"));
+array_push($servers, array('nome' => "COS702",			'url' => "cos702.bigbank.com.br",   	'agent' => "/monitor.php", 'protocol' => "http"));
+array_push($servers, array('nome' => "COS703",			'url' => "cos703.bigbank.com.br",  		'agent' => "/monitor.php", 'protocol' => "http"));
+array_push($servers, array('nome' => "COS704",			'url' => "cos704.bigbank.com.br",   	'agent' => "/monitor.php", 'protocol' => "http"));
+array_push($servers, array('nome' => "Py",				'url' => "py.bigbank.com.br",   		'agent' => "/monitor.php", 'protocol' => "http"));
+array_push($servers, array('nome' => "DEV",				'url' => "dev.bigbank.com.br",  		'agent' => "/monitor.php", 'protocol' => "http"));
+array_push($servers, array('nome' => "eSSe",            'url' => "www.esseregalos.com",         'agent' => "/monitor.php", 'protocol' => "http"));
+array_push($servers, array('nome' => "Utopias",         'url' => "utopiasargentinas.com",       'agent' => "/monitor.php", 'protocol' => "http"));
 
 echo json_encode($servers);
 
+*/
+echo file_get_contents('https://app.bigbank.com.br/servers.txt');
 ?>
